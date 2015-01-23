541fbfa20732eb5fadeb311e6ecda793a80d1c71
simpo <me@simpo.org>
2015-01-23 20:44:47 +0000

    Changed the test format and added tests for group delete and get all groups.  Also added helper functions for add and series of groups and deleting all test groups.

--- tests/indexSpec.js
+++ tests/indexSpec.js
@@ -11,6 +11,56 @@ function isProperty(obj, key) {
   return false;
 }
 
+function createTestGroups(quanity, api, callback){
+  var groupName = "Jasmine Test Group";
+  var groupDescription = "Unit Test Group for Jasmine";
+  var count = 0;
+
+  function done(body){
+    count++;
+    if(count >= quanity){
+      callback();
+    }
+  }
+
+  for(var n=1; n<=quanity; n++){
+    api.insertGroup(
+      "jasmine-test"+n+"@"+api.domain,
+      groupName+": "+n,
+      groupDescription+" ("+n+")",
+      done
+    );
+  }
+}
+
+function deleteTestGroups(api, callback){
+  var toDelete = [];
+  var count = 0;
+
+  function done(body){
+    count++;
+    if(count >= toDelete.length){
+      callback();
+    }
+  }
+
+  api.getAllGroups(function(allgroups){
+    allgroups.groups.forEach(function(group){
+      if(/jasmine\-test\d\@/.test(group.email)){
+        toDelete.push(group.email);
+      }
+    });
+  });
+
+  if(toDelete.length > 0){
+    toDelete.forEach(function(email){
+      api.deleteGroup(email, done);
+    });
+  }else{
+    callback();
+  }
+}
+
 describe('Test Api',function(){
 
   describe("Test api setup", function() {
@@ -33,10 +83,10 @@ describe('Test Api',function(){
     it("Api is created from input object", function(done){
       groupsApi({}, function(api){
 
-        expect(isObject(api)).toBe(true);
+        expect(isObject(api)).toBeTruthy();
 
         testProps.forEach(function(prop){
-          expect(isProperty(api, prop)).toBe(true);
+          expect(isProperty(api, prop)).toBeTruthy();
         });
 
         done();
@@ -46,10 +96,10 @@ describe('Test Api',function(){
     it("Api is created from input object", function(done){
       groupsApi('./tests/blank.json', function(api){
 
-        expect(isObject(api)).toBe(true);
+        expect(isObject(api)).toBeTruthy();
 
         testProps.forEach(function(prop){
-          expect(isProperty(api, prop)).toBe(true);
+          expect(isProperty(api, prop)).toBeTruthy();
         });
 
         done();
@@ -60,13 +110,52 @@ describe('Test Api',function(){
 
   it("Insert group", function(done) {
     groupsApi('./tests/options.json', function(api){
+      var groupId = "jasmine-test@"+api.domain;
+      var groupName = "Jasmine Test Group";
+      var groupDescription = "Unit Test Group for Jasmine";
 
-      api.insertGroup("jasmine-test@"+api.domain, "Jasmine Test Group", "Unit Test Group for Jasmine", function(body){
-        expect(isProperty(body, "error")).toBe(false);
+      api.insertGroup(groupId, groupName, groupDescription, function(body){
+        expect(isProperty(body, "error")).not.toBeTruthy();
+        expect(body.email).toEqual(groupId);
+        expect(body.name).toEqual(groupName);
+        expect(body.description).toEqual(groupDescription);
+
         done();
       });
 
     });
   });
+
+  it("Delete group", function(done) {
+    groupsApi('./tests/options.json', function(api){
+
+      api.deleteGroup("jasmine-test@"+api.domain, function(body){
+        expect(isProperty(body, "error")).not.toBeTruthy();
+
+        done();
+      });
+
+    });
+  });
+
+  it("Get all groups", function(done) {
+    groupsApi('./tests/options.json', function(api){
+      createTestGroups(3, api, function(){
+        api.getAllGroups(function(allgroups){
+          expect(allgroups.groups.length).toBeGreaterThan(2);
+
+          var testGroupCount = 0;
+          allgroups.groups.forEach(function(group){
+            if(/jasmine\-test\d\@/.test(group.email)){
+              testGroupCount++;
+            }
+          });
+          expect(testGroupCount).toEqual(3);
+
+          deleteTestGroups(api, done);
+        });
+      });
+    });
+  }, 15*1000);
 
 });
